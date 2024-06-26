e09a0844534a96ea113048ebb3a72eace43b164c
LeNgocBaThong <ngunguoi0909@gmail.com>
2024-06-26 12:59:03 +0700

    Update file

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,31 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+function openForm() {
+    document.getElementById("voucherForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("voucherForm").style.display = "none";
+}
+
+function navigateTo(url) {
+    window.location.href = url;
+}
+
+function loadHTML(element, url) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        if (this.status === 200) {
+            element.innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadHTML(document.querySelector("header"), "header.html");
+    loadHTML(document.querySelector("footer"), "footer.html");
+});
